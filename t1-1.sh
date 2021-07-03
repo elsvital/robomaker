d0e6516c3498e8ce8ff804f0cf7d9c9a8c1ff3a6
elsvital <elsvital@gmail.com>
2021-07-03 15:12:08 -0300

    change t1-1

--- t1-1.sh
+++ t1-1.sh
@@ -2,4 +2,4 @@
 echo 'init t1-1'
 cd aws-robomaker-sample-application-cloudwatch/robot_ws/ &&
 source install/setup.bash &&
-roslaunch cloudwatch_robot await_commands.launch &&
+roslaunch cloudwatch_robot await_commands.launch
